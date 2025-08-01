e92630be3e9805f0e74bdfbb7b6748639771176d
Cat Lazy <catlazy@Cats-MacBook-Pro.local>
2025-08-01 13:12:37 +0700

    avc

--- libraries/lib/steps/build.groovy
+++ libraries/lib/steps/build.groovy
@@ -1,7 +1,7 @@
 void call() {
     stage('Build') {
         // Check if the build step is enabled
-        if (env.BUILD_STEP_ENABLED == 'true') {
+        if (env.BUILD_STEP_ENABLED != 'true') {
             // Execute the build script
             echo "🔧 Building application..."
             sh "echo 'Compiling source code...'"
