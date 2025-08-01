8ed153f56341ddc09ad0a97cf9fc73ef48ee166d
Cat Lazy <catlazy@Cats-MacBook-Pro.local>
2025-08-01 11:34:47 +0700

    viet anh dau bui

--- libraries/lib/steps/build.groovy
+++ libraries/lib/steps/build.groovy
@@ -1,4 +1,12 @@
 void call() {
-    echo "🔧 Building application..."
-    sh "echo 'Compiling source code...'"
+    stage('Build') {
+        // Check if the build step is enabled
+        if (env.BUILD_STEP_ENABLED == 'true') {
+            // Execute the build script
+            echo "🔧 Building application..."
+            sh "echo 'Compiling source code...'"
+        } else {
+            echo 'Build step is disabled, skipping...'
+        }
+    }
 }
