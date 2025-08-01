ce30c8eda206a74057218fd107f3df7441ff53c4
Hymeno-Ran <jaydenthai16@gmail.com>
2025-08-01 10:20:59 +0700

    Update pipeline_config.groovy
    Signed-off-by: Hymeno-Ran <jaydenthai16@gmail.com>

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,12 +1,6 @@
-
-pipeline_template = "test-template-ci"
-
-libraries{
-    merge = true
-    lib
-}
-
-stages {
-    build()
-    deploy()
+fields {
+    required {
+        
+    }
+    optional {}
 }
