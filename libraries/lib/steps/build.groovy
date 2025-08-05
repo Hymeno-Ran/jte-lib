a09f5348376630974a14062da11d6767a75b0b68
Cat Lazy <catlazy@Cats-MacBook-Pro.local>
2025-08-05 20:14:19 +0700

    update some parameter

--- libraries/lib/steps/build.groovy
+++ libraries/lib/steps/build.groovy
@@ -1,14 +1,16 @@
 void call(String a, String b, String c, String d) {
     stage('Build') {
-        echo "${WORKSPACE}"
-        pre_build(a, b, c, d)
-        // Check if the build step is enabled
-        if (env.BUILD_STEP_ENABLED != 'true') {
-            // Execute the build script
-            echo "🔧 Building application..."
-            // sh "echo 'Compiling source code...'"
-        } else {
-            echo 'Build step is disabled, skipping...'
+        steps {
+            echo "${WORKSPACE}"
+            pre_build(a, b, c, d)
+            // Check if the build step is enabled
+            if (env.BUILD_STEP_ENABLED != 'true') {
+                // Execute the build script
+                echo "🔧 Building application..."
+                // sh "echo 'Compiling source code...'"
+            } else {
+                echo 'Build step is disabled, skipping...'
+            }
         }
     }
 }
