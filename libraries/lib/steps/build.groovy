3fa25d1ac79f10ad3bcbd52738c0750ff1ad78a3
Cat Lazy <catlazy@Cats-MacBook-Pro.local>
2025-08-12 19:40:20 +0700

    update agent

--- libraries/lib/steps/build.groovy
+++ libraries/lib/steps/build.groovy
@@ -1,24 +1,21 @@
 void call() {
     stage('Build') {
-        steps {
-            echo "${WORKSPACE}"
-            pre_build()
-            // Check if the build step is enabled
-            if (env.BUILD_STEP_ENABLED != 'true') {
-                // Execute the build script
-                echo "🔧 Building application..."
-                // sh "echo 'Compiling source code...'"
-            } 
-            else {
-                echo 'Build step is disabled, skipping...'
-            }
+        echo "${WORKSPACE}"
+        pre_build()
+        // Check if the build step is enabled
+        if (env.BUILD_STEP_ENABLED != 'true') {
+            // Execute the build script
+            echo "🔧 Building application..."
+            // sh "echo 'Compiling source code...'"
+        } 
+        else {
+            echo 'Build step is disabled, skipping...'
         }
+
     }
     stage('Run') {
-        steps {
-            echo "${WORKSPACE}"
-            pre_build(a, b, c, d)
-            echo "🔧 Building application..."
-        }
+        echo "${WORKSPACE}"
+        pre_build(a, b, c, d)
+        echo "🔧 Building application..."
     }
 }
