76545db03dfc7c76d2935fad7b019e81ee92afb7
Cat Lazy <catlazy@Cats-MacBook-Pro.local>
2025-08-18 22:02:50 +0700

    update stage

--- libraries/lib/steps/pre_build.groovy
+++ libraries/lib/steps/pre_build.groovy
@@ -1,3 +1,13 @@
 void call() {
     echo "✅ Running pre-build checks..."
+    stage('Pre-Build Checks') {
+        $class: 'GitSCM',
+        branches: [[name: '*/main']],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        userRemoteConfigs: [[
+            url: 'git@gitlab.com:devplus-huuchinh/requirement.git',
+            credentialsId: 'Git-get-agent'
+        ]]
+    }
 }
